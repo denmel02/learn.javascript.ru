896b4eaab5195af759ef6e1c758eb387f19d266a
Denis Melnikov <denis.melnikov@stm-labs.ru>
2020-05-12 07:50:18 +0300

    Data Types/JSON

---
+++ 05.DataTypes/08.JSON/SerializeEventCircular/script.js
@@ -0,0 +1,29 @@
+let room = {
+    number: 23
+};
+
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [ { name: "Иванов" }, { name: "Петров"  } ],
+    place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(JSON.stringify(meetup, function replacer(key, value) {
+    if (key !== '' && value === meetup) {
+        return;
+    }
+
+    return value;
+}));
+
+/* в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
+}
+*/
